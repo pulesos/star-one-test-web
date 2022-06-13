4085be8ec1ab53853cce57046ddb2544144691f8
Eugene <evgen7661@gmail.com>
2022-06-13 13:54:26 +0300

    added timer

--- src/components/CurrentEvents/CurrentEventsItem/CurrentEventsItem.jsx
+++ src/components/CurrentEvents/CurrentEventsItem/CurrentEventsItem.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react'
 import './CurrentEventsItem.scss'
 
 const CurrentEventsItem = () => {
+   const [timeLeft, setTimeLeft] = useState(5 * 60)
+
+   const getPadTime = (time) => time.toString().padStart(2, '0')
+
+   const minutes = getPadTime(Math.floor(timeLeft / 60))
+   const seconds = getPadTime(timeLeft - minutes * 60)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeLeft((timeLeft) => (timeLeft >= 1 ? timeLeft - 1 : 5 * 60))
+        }, 1000)
+        return () => clearInterval(interval)
+    }, [])
+
+
     const items = [
         {id: 1, title: 'Apple iPhone 13 Pro Max 256Gb (небесно-голубой)', avatar: 'https://cdn-icons-png.flaticon.com/512/147/147144.png'},
         {id: 2, title: '500 Stars', avatar: 'https://cdn-icons-png.flaticon.com/512/147/147144.png'},
         {id: 3, title: 'Sony PlayStation 5 Digital Edition  ', avatar: 'https://cdn-icons-png.flaticon.com/512/147/147144.png'}
-
     ]
     return (
         <>
@@ -23,7 +38,9 @@ const CurrentEventsItem = () => {
                         </div>
                         
                         <div className="current__events__timer">
-                            <span>00:00</span>
+                            <span>{minutes}</span>
+                            <span>:</span>
+                            <span>{seconds}</span>
                         </div>
                         
                         <button className="btn current__events__btn-green">СДЕЛАТЬ ХОД</button>
