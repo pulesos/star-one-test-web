f91dea45da39e2e5080d22e3e37a903315e1dabf
Eugene <evgen7661@gmail.com>
2022-07-08 00:28:16 +0300

    fixed modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -40,8 +40,6 @@ const Modal = ({modalActive, setModalActive, user, setUser, setLoggedIn, handleS
         loginUsers.setLogin(loginUsers)
         loginUsers.setIsAuth(true)
         console.log(response)
-        
-
     }
 
     const logIn = async() => {
@@ -52,21 +50,21 @@ const Modal = ({modalActive, setModalActive, user, setUser, setLoggedIn, handleS
         console.log(response)
     }
 
-    useEffect(() => {
-        /* global google*/
+    // useEffect(() => {
+    //     /* global google*/
 
-        google.accounts.id.initialize({
-            client_id: "567316206910-0fa69ilssoldc88j634bq53v5eo4tsk0.apps.googleusercontent.com",
-            callback: handleCallbackResponse
-        })
+    //     google.accounts.id.initialize({
+    //         client_id: "567316206910-0fa69ilssoldc88j634bq53v5eo4tsk0.apps.googleusercontent.com",
+    //         callback: handleCallbackResponse
+    //     })
 
-        google.accounts.id.renderButton(
-            document.getElementById("signInDiv"),
-            {theme: "outline", size: "large"}
-        )
+    //     google.accounts.id.renderButton(
+    //         document.getElementById("signInDiv"),
+    //         {theme: "outline", size: "large"}
+    //     )
 
-        google.accounts.id.prompt()
-    }, [])
+    //     google.accounts.id.prompt()
+    // }, [])
 
     return (
         <div className={modalActive ? 'modal active' : 'modal'} onClick={() => setModalActive(false)}>
@@ -105,11 +103,11 @@ const Modal = ({modalActive, setModalActive, user, setUser, setLoggedIn, handleS
                             <img src={facebook} height='25' alt='facebook' className='btn__img'/>
                             Войти по номеру телефона
                         </button>
-                        {/* <a className='btn btn-light'>
-                            <img src={google} height="25" alt="google" className='btn__img'/> 
+                        <a className='btn btn-light'>
+                            <img src={googlePic} height="25" alt="google" className='btn__img'/> 
                             <span className="googl2">Войти с помощью Google</span>
-                        </a> */}
-                        <div id="signInDiv"></div>
+                        </a>
+                        {/* <div id="signInDiv"></div> */}
                         {/* { Object.keys(user).length != 0 &&
                             <button onClick={(e) => handleSignOut(e)}>Выйти</button>
                         }
@@ -118,7 +116,7 @@ const Modal = ({modalActive, setModalActive, user, setUser, setLoggedIn, handleS
                                 <h3>{user.name}</h3>
                             </div>
                         } */}
-                        <button className='btn btn-dark'>
+                        <button className='btn btn-dark' disabled>
                             <img src={apple} height="25" alt="apple" className='btn__img'/>
                             Войти с помощью email
                         </button>
